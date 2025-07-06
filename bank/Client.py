7ae6cc98c3307dd9b7155c4d397663619b6e7de2
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-06 13:58:11 +0200

    Add minimal implementation of depositing method in the Client class

--- bank/Client.py
+++ bank/Client.py
@@ -1,4 +1,15 @@
+from decimal import Decimal
+
+
 class Client:
-    def __init__(self, name, balance):
+    def __init__(self, name, balance: Decimal):
         self.name = name
+
+        if balance < 0:
+            raise ValueError("Balance should be greater than 0")
         self.balance = balance
+
+    def depositing(self, deposit: Decimal):
+        self.balance += deposit
+        return deposit
+
