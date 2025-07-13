80decc8eb33af5083076887c2940da139a6cb9ca
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-13 22:58:22 +0200

    add logger when value of balance is negative

--- bank/Client.py
+++ bank/Client.py
@@ -1,12 +1,18 @@
 from decimal import Decimal
 
+import logging, logging.config
+
+logging.config.fileConfig("../logging.ini")
+logger = logging.getLogger('Client')
 
 class Client:
+
     def __init__(self, name, balance: Decimal):
         self.name = name
 
         if balance < 0:
-            raise ValueError("Balance should be greater than 0")
+            logger.error(f"Current value of balance {balance} cannot be less than 0.00")
+            raise ValueError
         self.balance = balance
 
     def depositing(self, deposit: Decimal):
