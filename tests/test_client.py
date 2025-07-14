fb1221840ae87e54810284b04544305395a3ba32
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-14 23:50:31 +0200

    Refactor the test to static methods

--- tests/test_client.py
+++ tests/test_client.py
@@ -16,33 +16,23 @@ init_invalid_balance_data = [None, "Hello world!", Decimal("-0.01")]
 
 
 @pytest.mark.parametrize("name", init_valid_name_data)
-@pytest.mark.parametrize("balance", init_valid_balance_data)
-def test_init_valid_balance_validation(name, balance):
-    client = Client(name, balance)
-
-    assert client.balance == balance
-
-
-@pytest.mark.parametrize("name", init_valid_name_data)
-@pytest.mark.parametrize("balance", init_invalid_balance_data)
-def test_init_invalid_balance_validation(name, balance):
-    if not isinstance(balance, Decimal):
-        with pytest.raises(TypeError):
-            Client(name, balance)
-    elif balance < Decimal("0.00"):
-        with pytest.raises(ValueError):
-            Client(name, balance)
+def test_valid_name_validation(name):
+    assert Client.validate_name(name) is True
 
 
 @pytest.mark.parametrize("name", init_invalid_name_data)
+def test_invalid_name_validation(name):
+    assert Client.validate_name(name) is False
+
+
 @pytest.mark.parametrize("balance", init_valid_balance_data)
-def test_init_invalid_name_validation(name, balance):
-    if not isinstance(name, str):
-        with pytest.raises(TypeError):
-            Client(name, balance)
-    elif not name.isalpha():
-        with pytest.raises(ValueError):
-            Client(name, balance)
+def test_valid_balance_validation(balance):
+    assert Client.validate_balance(balance) is True
+
+
+@pytest.mark.parametrize("balance", init_invalid_balance_data)
+def test_invalid_balance_validation(balance):
+    assert Client.validate_balance(balance) is False
 
 
 @pytest.mark.parametrize("name", init_valid_name_data)
