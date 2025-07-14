39c3f8687a9f631e9615d58ae9b1d31fe3b1487c
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-14 22:06:17 +0200

    add validation of name and balance

--- bank/Client.py
+++ bank/Client.py
@@ -1,18 +1,34 @@
+import logging.config
 from decimal import Decimal
 
-import logging, logging.config
-
 logging.config.fileConfig("../logging.ini")
-logger = logging.getLogger('Client')
+logger = logging.getLogger("Client")
+
 
 class Client:
-
-    def __init__(self, name, balance: Decimal):
-        self.name = name
-
-        if balance < 0:
-            logger.error(f"Current value of balance {balance} cannot be less than 0.00")
+    def __init__(self, name, balance):
+        if not name.isalpha():
+            logger.error(f'Provided name "{name}" must contain only letters')
             raise ValueError
+
+        logger.debug("Name passed validation")
+
+        if not isinstance(balance, Decimal):
+            logger.error(
+                f"The provided balance {balance} is not in a valid currency format"
+            )
+            raise TypeError
+        elif balance < Decimal("0.00"):
+            logger.error(
+                f"Current value of balance {balance} cannot be less than 0.00"
+            )
+            raise ValueError
+
+        logger.debug("Balance passed validation")
+
+        logger.info("Successfully initialized Client class")
+
+        self.name = name
         self.balance = balance
 
     def depositing(self, deposit: Decimal):
