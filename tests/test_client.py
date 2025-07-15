669683f4784af7c65c27bd805abc753f3aa1f56e
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-15 07:12:27 +0200

    add testcases for class initialization

--- tests/test_client.py
+++ tests/test_client.py
@@ -37,6 +37,30 @@ def test_invalid_balance_validation(balance):
 
 @pytest.mark.parametrize("name", init_valid_name_data)
 @pytest.mark.parametrize("balance", init_valid_balance_data)
+def test_init_valid_client(name, balance):
+    client = Client(name, balance)
+
+    assert isinstance(client, Client)
+    assert client.name == name
+    assert client.balance == balance
+
+
+@pytest.mark.parametrize("name", init_invalid_name_data)
+@pytest.mark.parametrize("balance", init_valid_balance_data)
+def test_init_invalid_name_client(name, balance):
+    with pytest.raises(ValueError):
+        Client(name, balance)
+
+
+@pytest.mark.parametrize("name", init_valid_name_data)
+@pytest.mark.parametrize("balance", init_invalid_balance_data)
+def test_init_invalid_balance_client(name, balance):
+    with pytest.raises(ValueError):
+        Client(name, balance)
+
+
+@pytest.mark.parametrize("name", init_valid_name_data)
+@pytest.mark.parametrize("balance", init_valid_balance_data)
 def test_depositing(name, balance):
     client = Client(name, balance)
 
