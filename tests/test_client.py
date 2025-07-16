0d3bfe7331e876ecdb34b102ca5e2b9b013ec6a3
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-16 21:27:45 +0200

    refactoring of testcases

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,6 +4,9 @@ import pytest
 
 from bank.Client import Client
 
+init_name = "Anna"
+init_balance = Decimal("10.11")
+
 init_valid_name_data = ["Adam", "John", "Lisa"]
 init_invalid_name_data = ["", "     ", "!@#$%>", 0, None]
 
@@ -17,22 +20,22 @@ init_invalid_monetary_data = [None, "Hello world!", Decimal("-0.01")]
 
 @pytest.mark.parametrize("name", init_valid_name_data)
 def test_valid_name_validation(name):
-    assert Client.validate_name(name) is True
+    assert Client.name_validation(name) is True
 
 
 @pytest.mark.parametrize("name", init_invalid_name_data)
 def test_invalid_name_validation(name):
-    assert Client.validate_name(name) is False
+    assert Client.name_validation(name) is False
 
 
 @pytest.mark.parametrize("balance", init_valid_monetary_data)
 def test_valid_balance_validation(balance):
-    assert Client.validate_monetary_values(balance) is True
+    assert Client.monetary_values_validation(balance) is True
 
 
 @pytest.mark.parametrize("balance", init_invalid_monetary_data)
 def test_invalid_balance_validation(balance):
-    assert Client.validate_monetary_values(balance) is False
+    assert Client.monetary_values_validation(balance) is False
 
 
 @pytest.mark.parametrize("name", init_valid_name_data)
@@ -61,29 +64,25 @@ def test_init_invalid_balance_client(name, balance):
 
 @pytest.mark.parametrize("name", init_valid_name_data)
 def test_anonymize_name(name):
-    anonymized_name = Client.anonymized_name()
+    anonymized_name = Client.anonymize_name()
 
     assert isinstance(anonymized_name, str)
     assert anonymized_name != name
 
 
-@pytest.mark.parametrize("name", init_valid_name_data)
-@pytest.mark.parametrize("balance", init_valid_monetary_data)
 @pytest.mark.parametrize("deposit", init_valid_monetary_data)
-def test_depositing(name, balance, deposit):
-    client = Client(name, balance)
+def test_valid_data_epositing(deposit):
+    client = Client(init_name, init_balance)
 
     result = client.depositing(deposit)
 
-    assert client.balance == balance + deposit
+    assert client.balance == init_balance + deposit
     assert result == deposit
 
 
-@pytest.mark.parametrize("name", init_valid_name_data)
-@pytest.mark.parametrize("balance", init_valid_monetary_data)
 @pytest.mark.parametrize("deposit", init_invalid_monetary_data)
-def test_depositing(name, balance, deposit):
-    client = Client(name, balance)
+def test_invalid_data_depositing(deposit):
+    client = Client(init_name, init_balance)
 
     with pytest.raises(ValueError):
         client.depositing(deposit)
