bc3132546ea1e58779d9b3071e63991f0cd3caa2
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-16 21:28:16 +0200

    add log decorator

--- bank/Client.py
+++ bank/Client.py
@@ -1,3 +1,4 @@
+import functools
 import logging.config
 import random
 import string
@@ -7,29 +8,56 @@ logging.config.fileConfig("../logging.ini")
 logger = logging.getLogger("Client")
 
 
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        logger.debug(f"Starting {func.__name__.replace('_', ' ')}")
+        operation_result = False
+        try:
+            result = func(*args, **kwargs)
+            operation_result = True
+            return result
+            logger.debug(
+                f"Successfully done {func.__name__.replace('_', ' ')}"
+            )
+        except Exception as e:
+            logger.error(
+                f"Exception raised in {func.__name__}. Exception: {str(e)}"
+            )
+            raise e
+        finally:
+            logger.debug(
+                f"Ending {func.__name__.replace('_', ' ')} with the "
+                f"result: {operation_result}"
+            )
+
+    return wrapper
+
+
 class Client:
     def __init__(self, name, balance):
         logger.debug("Starting Client class initialization")
-        if not Client.validate_name(name):
+        if not Client.name_validation(name):
             logger.error("Invalid name provided to the constructor")
             raise ValueError("Invalid name")
 
-        if not Client.validate_monetary_values(balance):
+        if not Client.monetary_values_validation(balance):
             logger.error("Invalid balance provided to the constructor")
             raise ValueError("Invalid balance")
 
         self.name = name
         self.balance = balance
-        self.anonymized_name = Client.anonymized_name()
+        self.anonymized_name = Client.anonymize_name()
         self.anonymized_monetary_value = "***"
 
         logger.info(
             f"Successfully initialized Client class for name "
-            f"{self.anonymized_name} and balance {self.anonymized_monetary_value}"
+            f"{self.anonymized_name} and balance "
+            f"{self.anonymized_monetary_value}"
         )
 
     def depositing(self, deposit):
-        if not Client.validate_monetary_values(deposit):
+        if not Client.monetary_values_validation(deposit):
             logger.error("Invalid amount of deposit")
             raise ValueError("Provided invalid deposit")
         self.balance += deposit
@@ -37,69 +65,36 @@ class Client:
         return deposit
 
     @staticmethod
-    def validate_name(name):
-        logger.debug("Starting name validation")
-        validation_result = False
-        try:
-            if not isinstance(name, str):
-                logger.error("Provided name value is not a string")
-                return False
-            if not name:
-                logger.error("Provided name value cannot be empty or None")
-                return False
-            elif not name.isalpha():
-                logger.error("Provided name must contain only letters")
-                return False
-            logger.debug("Provided name passed validation")
-            validation_result = True
-            return True
-        except Exception as e:
-            logger.error(f"Unexpected error during name validation: {e}")
-        finally:
-            logger.debug(
-                f"Finished name validation with result: {validation_result}"
-            )
+    @log
+    def name_validation(name):
+        if not isinstance(name, str):
+            logger.error("Provided name value is not a string")
+            return False
+        if not name:
+            logger.error("Provided name value cannot be empty or None")
+            return False
+        elif not name.isalpha():
+            logger.error("Provided name must contain only letters")
+            return False
+        return True
 
     @staticmethod
-    def validate_monetary_values(monetary_value):
-        logger.debug("Starting monetary values validation")
-        validation_result = False
-        try:
-            if not isinstance(monetary_value, Decimal):
-                logger.error(
-                    "The provided monetary value is not in a valid currency format"
-                )
-                return False
-            elif monetary_value < Decimal("0.00"):
-                logger.error("Monetary value cannot be less than 0.00")
-                return False
-            logger.debug("Provided monetary value passed validation")
-            validation_result = True
-            return True
-        except Exception as e:
+    @log
+    def monetary_values_validation(monetary_value):
+        if not isinstance(monetary_value, Decimal):
             logger.error(
-                f"Unexpected error during monetary values validation: {e}"
+                "The provided monetary value is not in a valid currency format"
             )
-        finally:
-            logger.debug(
-                f"Finished monetary values validation with result: {validation_result}"
-            )
+            return False
+        elif monetary_value < Decimal("0.00"):
+            logger.error("Monetary value cannot be less than 0.00")
+            return False
+        return True
 
     @staticmethod
-    def anonymized_name():
-        logger.debug("Starting anonymizing name")
-        anonymizing_result = False
-        try:
-            logger.debug("Starting anonymizing name")
-            result = "".join(
-                random.choices(string.ascii_letters + string.digits, k=15)
-            )
-            logger.debug(f"Anonymizing name ended with result: {result}")
-            anonymizing_result = True
-            return result
-        except Exception as e:
-            logger.error(f"Unexpected error during anonymizing name: {e}")
-        finally:
-            logger.debug(
-                f"Finished anonymizing name with result: {anonymizing_result}"
-            )
+    @log
+    def anonymize_name():
+        result = "".join(
+            random.choices(string.ascii_letters + string.digits, k=15)
+        )
+        return result
