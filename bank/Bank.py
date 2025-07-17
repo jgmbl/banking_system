01bfdbda8be3fe553c5034dd9379e178852a2f6b
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-17 23:03:05 +0200

    add add_client method

--- bank/Bank.py
+++ bank/Bank.py
@@ -11,7 +11,18 @@ class Bank:
         logger.debug("Starting client class initialization")
         if isinstance(clients, Client):
             self.clients = list(clients)
-        else:
-            self.clients = clients
+
+        self.clients = clients
 
         logger.info("Successfully initialized Bank object")
+
+    def add_client(self, client: Client):
+        logger.debug("Initialized adding client process")
+        if not client:
+            logger.error("Provided None value or empty datatype")
+            raise ValueError("Provided client object cannot be empty")
+        if not isinstance(client, Client):
+            logger.error("Provided invalid datatype")
+            raise TypeError("Client must be an instance of Client class")
+
+        self.clients.append(client)
