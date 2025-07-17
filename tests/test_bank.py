028de773c03bd8cb281fffca77ffbf8239fc89a1
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-17 20:09:45 +0200

    modify sorting of the lists in the testcase

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -3,22 +3,27 @@ from decimal import Decimal
 import pytest
 
 from bank.Bank import Bank
-
 from bank.Client import Client
 
 INIT_CLIENTS = [
-    [Client("Anna", Decimal("0.00")), Client("John"), Decimal("1000.00")],
-    [Client("Robert", Decimal("12.12"))]]
+    [Client("Anna", Decimal("0.00")), Client("John", Decimal("1000.00"))],
+    [Client("Robert", Decimal("12.12"))],
+]
 
 
 @pytest.mark.parametrize("clients", INIT_CLIENTS)
 def test_init_bank(clients):
     bank = Bank(clients)
 
-    bank.clients.sort()
-    clients.sort()
+    if len(bank.clients) == 1:
+        assert bank.clients == clients
+
+    bank.clients.sort(key=lambda n: n.name)
+    clients.sort(key=lambda n: n.name)
+
     assert isinstance(bank, Bank)
     assert bank.clients == clients
+
 
 def test_init_bank_no_clients():
     clients = []
