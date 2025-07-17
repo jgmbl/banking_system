85f1bea38e7473324f233930819816429aaa9289
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-17 23:16:08 +0200

    create decorators module with log decorator

---
+++ utils/decorators.py
@@ -0,0 +1,24 @@
+import logging.config
+from functools import wraps
+
+logging.config.fileConfig("../logging.ini")
+
+
+def log(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger_name = func.__module__
+        logger = logging.getLogger(str(logger_name).split('.')[-1])
+        logger.debug(f"Starting {func.__name__.replace('_', ' ')}")
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            logger.error(
+                f"Exception raised in {func.__name__}. Exception: {str(e)}"
+            )
+            raise e
+        finally:
+            logger.debug(f"Finished {func.__name__.replace('_', ' ')}")
+
+    return wrapper
