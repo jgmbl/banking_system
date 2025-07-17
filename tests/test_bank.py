c6a3551bd7b485408024d4246192364f4cb9df44
Joanna Gembel-Stojek <joannagembel@gmail.com>
2025-07-18 00:26:16 +0200

    add testcases for client removal

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -13,7 +13,7 @@ INIT_CLIENTS = [
     Client("Elisabeth", Decimal("1000000.00")),
 ]
 
-INIT_IVALID_CLIENTS = [None, "", True, 0, []]
+INIT_INVALID_CLIENTS = [None, "", True, 0, []]
 
 
 def test_init_bank():
@@ -44,9 +44,37 @@ def test_add_client():
     assert len(bank.clients) == len(INIT_CLIENTS) + 1
 
 
-@pytest.mark.parametrize("client", INIT_IVALID_CLIENTS)
+@pytest.mark.parametrize("client", INIT_INVALID_CLIENTS)
 def test_add_empty_client(client):
     bank = Bank(INIT_CLIENTS)
 
     with pytest.raises((TypeError, ValueError)):
         bank.add_client(client)
+
+
+def test_remove_client():
+    initial_clients = INIT_CLIENTS.copy()
+    bank = Bank(initial_clients)
+
+    clients_id_list = []
+    for client in bank.clients:
+        clients_id_list.append(client.id)
+
+    client_id_list_expected = clients_id_list.copy()
+    client_id_removed = client_id_list_expected[1]
+
+    removed_client_exists = any(
+        client.id == client_id_removed for client in bank.clients
+    )
+
+    bank.remove_client(client_id_removed)
+
+    assert len(bank.clients) != len(initial_clients)
+    assert not removed_client_exists
+
+
+def test_remove_non_existent_client():
+    bank = Bank([])
+
+    with pytest.raises(ValueError):
+        bank.remove_client(10)
